a19a3805f5d4d9f04161fd93ec3926109578afb1
Quan1umMango <astondsouza369@gmail.com>
2025-11-15 18:21:28 +0530

    oopsie

--- constants.py
+++ constants.py
@@ -7,9 +7,13 @@ WHITE = 255,255,255
 MAROON = 128, 0, 0
 RED = 255,0,0
 DARK_GREEN =  0, 100,0 
-LIGHT_GREEN =  45, 207, 70
+LIGHT_GREEN = 90,180,80 
+GREEN_BG = LIGHT_GREEN 
+ACCENT = 255, 255, 255
+HOVER = 200, 255, 200
+BOX = 20, 90, 40
 
-BG_COLOR = 45,207,70
+BG_COLOR =    60,200,120 
 BLOCK_COLOR = DARK_GREEN
 
 # Game object consts
@@ -21,12 +25,39 @@ BORDER_SIZE = 30
 SQUARE_SIZE = 20
 
 BALL_RADIUS = 15 
-BALL_COLOR = RED
+HOLE_RADIUS = 30
+BALL_COLOR = WHITE 
 
 FRICTION = 0.01
 MAX_VELOCITY = 500 
 
 STEP_BY = 0.01
 
+
+# UI Constants
+BACKGROUND_IMAGE = './assets/iamge.png'
+MUSIC_ICON_PATH = './assets/icon_music.png'
+SFX_ICON_PATH = './assets/icon_sfx.png'
+CUSTOM_FONT_FILENAMES = ["game_font.ttf", "pixel_font.ttf", "arcade_font.ttf"]
+INTRO_DURATION = 3
+BUTTON_STAGGER = [1,1.5, 2]
+CLICK_PAD = 8
+ICON_SIZE = (40, 40)   # requested tiny icon size
+MAX_ZOOM = 1.06  # or 1.10 if you want more zoom
+MIN_ZOOM = 1.00
+current_zoom = MIN_ZOOM
+
+# UI geometry
+BUTTON_W, BUTTON_H = 380, 84
+BUTTON_X = (SCREEN_W - BUTTON_W) // 2
+PLAY_Y = 220
+LEVEL_Y = PLAY_Y + BUTTON_H + 18
+SETTINGS_Y = LEVEL_Y + BUTTON_H + 18
+
+# Animation
+ANIM_SPEED = 4.5
+
 # Misc
 MOUSE_BUTTON_ONE = 1
+
+HOVER_SPEED = 8.0
